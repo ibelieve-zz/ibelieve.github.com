bcf57ec4aae95835c0e31f1d37d91bef51b28bd1
Bing <magpie727@gmail.com>
2012-12-20 18:55:03 -0500

    change a little of confiure file

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,21 +2,35 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'Robin (Bing) Lu'
+
+
 SITENAME = u'Compile My Dream'
-SITEURL = ''
+SITEURL = 'http://ibelieve.github.com'
 
 TIMEZONE = 'America/Toronto'
 
+
+
+
 DEFAULT_LANG = u'en'
+
+#FEED_RSS = 'feeds/all.rss.xml'
+#CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+          ('Python.org', 'http://python.org'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+          ('github', 'http://github.com/ibelieve'),)
 
 DEFAULT_PAGINATION = 10
+
+GITHUB_URL = 'http://github.com/ibelieve'
+#NEWEST_FIRST_ARCHIVES (True)
+#DEFAULT_CATEGORY ('blog')
+#TYPOGRIFY (True)
+#SUMMARY_MAX_LENGTH (50)
